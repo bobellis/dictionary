0afc4d2565477794bf037cfb65e364ba80dc6eb9
Bob <bobellis.tx@gmail.com>
2015-05-01 09:45:27 -0700

    Add #save method and .all method to word class to pass the save spec

--- lib/word.rb
+++ lib/word.rb
@@ -8,4 +8,12 @@ class Word
     @definitions = []
   end
 
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -7,7 +7,7 @@ describe(Word) do
       it('will save a new word object into the array of words') do
         test_word = Word.new('test')
         test_word.save()
-        expect(Word.all()).to(eq(test_word))
+        expect(Word.all()).to(eq([test_word]))
     end
   end
 end
