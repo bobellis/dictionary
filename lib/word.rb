4f386fea49e17d4738230ed33826cfa69b40c80b
Bob <bobellis.tx@gmail.com>
2015-05-01 11:49:24 -0700

    Add a .find method to the word class which will search for a word by id and return it

--- lib/word.rb
+++ lib/word.rb
@@ -30,4 +30,15 @@ attr_reader(:spelling, :word_id)
     @@words = []
   end
 
+  define_singleton_method(:find) do |id|
+    found_word = ''
+    @@words.each do |word|
+      if word.word_id = id
+        found_word = word
+      end
+    end
+    found_word
+  end
+
+
 end
