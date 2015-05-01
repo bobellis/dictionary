137059860618bf6ba76e7ef284ae8d2120562702
Bob <bobellis.tx@gmail.com>
2015-05-01 10:05:50 -0700

    Add a spec to test #def_details which will return the contents of a definition

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,11 +8,23 @@ describe(Word) do
         test_word = Word.new('test')
         test_word.save()
         expect(Word.all()).to(eq([test_word]))
+      end
     end
-  end
+    describe('#spelling') do
+      it('will return the spelling property of a word') do
+        test_word = Word.new('test')
+        test_word.save()
+        expect(test_word.spelling()).to(eq('test'))
+      end
+    end
+
 end
 
-describe ('definition') do
-
-
+describe (Definition) do
+    describe('#def_details') do
+      it('will return the details of a definition') do
+        test_definition = Definition.new('this is a test')
+        expect(test_definition.def_details()).to(eq('this is a test'))
+      end
+    end
 end
